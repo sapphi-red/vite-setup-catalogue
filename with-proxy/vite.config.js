444ddcaa24c695ded5757dd83431ea89e11d9e27
sapphi-red <green@sapphi.red>
2022-06-21 20:21:49 +0900

    chore: use Vite 3.0.0-alpha.14

--- with-proxy/vite.config.js
+++ with-proxy/vite.config.js
@@ -4,9 +4,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    strictPort: true,
-    hmr: {
-      clientPort: 3000 // caddy 3000 -> vite 5173
-    }
+    strictPort: true
   }
 })
