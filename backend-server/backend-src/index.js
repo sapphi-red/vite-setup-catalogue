13aa8bbebd7fc330685ae4617f8f06f5deac95b8
sapphi-red <green@sapphi.red>
2022-06-26 00:22:00 +0900

    feat(backend-server): build setup

--- backend-server/backend-src/index.js
+++ backend-server/backend-src/index.js
@@ -4,19 +4,67 @@ import fs from 'fs/promises'
 import url from 'url'
 import ejs from 'ejs'
 
+const isDev = process.env.DEV === '1'
+
 const connect = Connect()
 
-const indexTemplatePath = url.fileURLToPath(new URL('./templates/index.ejs', import.meta.url))
+const entrypoint = 'frontend-src/main.js'
+
+let viteInject
+if (isDev) {
+  viteInject = `
+    <script type="module" src="http://localhost:5173/${entrypoint}"></script>
+  `
+} else {
+  const manifest = JSON.parse(
+    await fs.readFile(
+      new URL('../dist/manifest.json', import.meta.url),
+      'utf-8'
+    )
+  )
+  const main = manifest[entrypoint]
+
+  viteInject = `
+    ${main.css.map((href) => `<link rel="stylesheet" href="${href}" />`)}
+    <script type="module" src="${main.file}"></script>
+  `
+}
+
+connect.use(
+  serveStatic(url.fileURLToPath(new URL('./public', import.meta.url)), {
+    index: false
+  })
+)
+if (!isDev) {
+  const serve = serveStatic(
+    url.fileURLToPath(new URL('../dist', import.meta.url)),
+    {
+      immutable: true,
+      index: false
+    }
+  )
+  connect.use((req, res, next) => {
+    if (new URL(req.url, 'http://dummy').pathname === '/manifest.json') {
+      res.statusCode = 404
+      res.end()
+      return
+    }
+    serve(req, res, next)
+  })
+}
+
+const indexTemplatePath = url.fileURLToPath(
+  new URL('./templates/index.ejs', import.meta.url)
+)
 
 connect.use(async (req, res, next) => {
-  const content = await ejs.renderFile(indexTemplatePath, { data: 'foo' })
+  const content = await ejs.renderFile(indexTemplatePath, {
+    data: 'foo',
+    viteInject
+  })
   res.statusCode = 200
   res.end(content)
 })
-
-connect.use(
-  serveStatic(url.fileURLToPath(new URL('./public', import.meta.url)))
-)
 
 const server = connect.listen(3000, 'localhost')
 server.on('listening', () => {
