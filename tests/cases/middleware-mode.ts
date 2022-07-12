da78db03e0519f86c51fd705a7770cd343509d58
sapphi-red <green@sapphi.red>
2022-07-12 15:21:17 +0900

    test: add restart test

--- tests/cases/middleware-mode.ts
+++ tests/cases/middleware-mode.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test'
-import type { ChildProcessWithoutNullStreams } from 'child_process'
+import type { Page } from '@playwright/test'
 import { spawn } from 'cross-spawn'
 import {
   editFile,
@@ -14,29 +14,66 @@ import {
 const workspaceFileURL = getWorkspaceFileURL('middleware-mode')
 const accessURL = `http://localhost:${ports.middlewareMode}/`
 
-let viteDevProcess: ChildProcessWithoutNullStreams
-
-test.beforeAll(async () => {
-  viteDevProcess = spawn('pnpm', ['run', 'dev'], { cwd: workspaceFileURL })
+const startVite = async () => {
+  const viteDevProcess = spawn('pnpm', ['run', 'dev'], {
+    cwd: workspaceFileURL
+  })
   await collectAndWaitUntilOutput(
     viteDevProcess.stdout,
     viteDevProcess.stderr,
     'Open your browser.'
   )
+
+  return async () => {
+    try {
+      await killProcess(viteDevProcess)
+    } catch {}
+  }
+}
+
+const setupAndGotoPage = async (page: Page) => {
+  outputError(page)
+  await gotoAndWaitForHMRConnection(page, accessURL, { timeout: 1000 })
+}
+
+test('hmr test', async ({ page }) => {
+  const finishVite = await startVite()
+  try {
+    await setupAndGotoPage(page)
+
+    const title = page.locator('h1')
+    await expect(title).toHaveText('Hello Vite!')
+
+    await editFile('./src/main.js', workspaceFileURL, (content) =>
+      content.replace('Vite!</h1>', 'Vite!!!</h1>')
+    )
+
+    await expect(title).toHaveText('Hello Vite!!!')
+  } finally {
+    await finishVite()
+  }
 })
 
-test('middleware-mode test', async ({ page }) => {
-  outputError(page)
-  await gotoAndWaitForHMRConnection(page, accessURL, { timeout: 10000 })
+test('restart test', async ({ page }) => {
+  let finishVite1: (() => Promise<void>) | undefined
+  let finishVite2: (() => Promise<void>) | undefined
 
-  const title = page.locator('h1')
-  await expect(title).toHaveText('Hello Vite!')
+  try {
+    finishVite1 = await startVite()
+    await setupAndGotoPage(page)
 
-  await editFile('./src/main.js', workspaceFileURL, (content) =>
-    content.replace('Vite!</h1>', 'Vite!!!</h1>')
-  )
+    const navigationPromise = page.waitForNavigation()
 
-  await expect(title).toHaveText('Hello Vite!!!')
+    await finishVite1()
+    finishVite1 = undefined
+
+    finishVite2 = await startVite()
+
+    await navigationPromise
+  } finally {
+    await finishVite1?.()
+    await finishVite2?.()
+  }
 })
 
 test.afterAll(async () => {
@@ -44,8 +81,4 @@ test.afterAll(async () => {
   await editFile('./src/main.js', workspaceFileURL, (content) =>
     content.replace('Vite!!!</h1>', 'Vite!</h1>')
   )
-
-  try {
-    await killProcess(viteDevProcess)
-  } catch {}
 })
