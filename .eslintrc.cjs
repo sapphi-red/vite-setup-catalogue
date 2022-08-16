ba7a8d2d66edf018b0b150c408803d3e1485fd90
sapphi-red <green@sapphi.red>
2022-08-16 14:48:06 +0900

    chore: add linter/formatter

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest'
+  },
+  env: {
+    node: true
+  },
+  overrides: [
+    {
+      files: ['**/*.cjs'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    },
+    {
+      files: ['**/*.{js,mjs,cjs}'],
+      extends: ['eslint:recommended', 'prettier'],
+      rules: {
+        'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        'no-empty': ['error', { allowEmptyCatch: true }]
+      }
+    },
+    {
+      files: ['**/*.{ts,mts,cts}'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
+      ],
+      rules: {
+        'no-empty': ['error', { allowEmptyCatch: true }],
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
+    },
+    {
+      files: [
+        'examples/**',
+        'tests/examples-overrides/**',
+        'tests/fixtures/**'
+      ],
+      excludedFiles: 'vite.config.{js,ts}',
+      env: {
+        browser: true
+      }
+    }
+  ],
+  reportUnusedDisableDirectives: true
+}
