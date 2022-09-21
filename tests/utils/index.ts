b9d374f370dff6820e8ddec7fafa877fbb0a6760
sapphi-red <green@sapphi.red>
2022-09-21 16:12:51 +0900

    chore: fix format

--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -179,11 +179,9 @@ export const runDockerCompose = async (
 ): Promise<DockerComposeProcess> => {
   // ensure there isn't any conflict with container name
   await new Promise<void>(resolve => {
-    const downProcess = spawn(
-      'docker',
-      `compose ${options} down`.split(' '),
-      { cwd }
-    )
+    const downProcess = spawn('docker', `compose ${options} down`.split(' '), {
+      cwd
+    })
     downProcess.once('exit', () => {
       resolve()
     })
