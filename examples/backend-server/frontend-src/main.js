e668826a37dfd1aab52272a97746f480546bfe16
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-14 18:52:42 +0900

    fix: setup `server.origin` for backend integration

--- examples/backend-server/frontend-src/main.js
+++ examples/backend-server/frontend-src/main.js
@@ -4,4 +4,6 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <div>Test asset</div>
+  <img src="${new URL('./ok.png', import.meta.url).href}" alt="NOT OK" width="30" />
 `
