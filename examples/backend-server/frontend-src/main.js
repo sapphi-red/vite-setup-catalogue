9bf38b61d9164fcc9769e3a02e36d08d9ed727af
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-15 08:12:33 +0900

    chore: new URL was working, but import was not

--- examples/backend-server/frontend-src/main.js
+++ examples/backend-server/frontend-src/main.js
@@ -1,9 +1,12 @@
 import 'vite/modulepreload-polyfill'
 import './style.css'
+import okPng from "./ok.png";
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-  <div>Test asset</div>
+  <pre>import okPng from "./ok.png"</pre>
+  <img src="${okPng}" alt="NOT OK" width="30" />
+  <pre>new URL('./ok.png', import.meta.url).href</pre>
   <img src="${new URL('./ok.png', import.meta.url).href}" alt="NOT OK" width="30" />
 `
