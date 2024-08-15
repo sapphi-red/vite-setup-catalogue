08a789d973b393de5736c5dfaaab3219cc5e8967
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-15 19:09:11 +0900

    chore: vite.svg in all examples

--- examples/with-proxy/src/main.js
+++ examples/with-proxy/src/main.js
@@ -1,6 +1,8 @@
 import './style.css'
+import viteSvg from './vite.svg'
 
 document.querySelector('#app').innerHTML = `
+  <img src="${viteSvg}" alt="Vite Logo" width="60" />
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
